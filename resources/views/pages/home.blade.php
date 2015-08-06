0f1b72d0cac4378402923fa96a6828b8184887bb
hikups <jhoebeeck@gmail.com>
2015-08-07 00:13:01 +0100

    form model and migrations

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+
+      <div class="jumbotron">
+
+		<h1>Project Flyer</h1>
+        <p>
+	        Cras justo odio, dapibus ac facilisis in, egestas eget quam. 
+	        Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
+	        ut fermentum massa justo sit amet risus.
+        </p>
+
+        <a href="/flyers/create" class="btn btn-lg btn-success">Create a Flyer</a>
+        
+      </div>
+@stop
